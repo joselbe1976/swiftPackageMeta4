0b12470a5819c15f59ce2fe7e980c184836c7169
Jose Luis Bustos Esteban <jlbustos@equilibrha.es>
2020-06-25 13:49:16 +0200

    second cahcneg api

--- Sources/MyLibrary/Models.swift
+++ Sources/MyLibrary/Models.swift
@@ -9,6 +9,19 @@ import Foundation
 
 
 public struct empleadoMeta4 {
-    var name : String
-    var id : String
+    public var name : String
+    public var id : String
+    
+    public init(name: String, id: String){
+        self.name = name
+        self.id = id
+    }
+    
+    
+    public func getName() -> String {
+        return self.name
+    }
+    private func clcIdioma() -> String {
+        return "idioma"
+    }
 }
